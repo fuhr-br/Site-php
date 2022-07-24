70e1918e0f569e0fdd84e9b5e0a2f5cada3494aa
Anderson Fuhr Souza <anderson.souza@dbserver.com.br>
2022-07-24 20:42:13 -0300

    comentar metodo envio de email

--- site/php/notificacoes.php
+++ site/php/notificacoes.php
@@ -3,9 +3,9 @@ $email=$_POST['email'];
 $mensagem ='Olá seja bem vindo ao Vida Alternativa!';
 $assunto = "Noticias"
 mail($email,$assunto,$mensagem);	
-if (!$mail->send()) {
-	echo "Falha ao enviar o email: " . $mail->ErrorInfo;
-} else {
-	echo "Mensagem enviada com Sucesso!";
-}
+//if (!$mail->send()) {
+//	echo "Falha ao enviar o email: " . $mail->ErrorInfo;
+//} else {
+//	echo "Mensagem enviada com Sucesso!";
+//}
   ?>
